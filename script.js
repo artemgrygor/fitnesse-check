7a827f3abec6b407a104fa890fcef20f24fbaa9e
Artem Grygor <artemgrygor@gmail.com>
2014-11-14 13:25:18 -0500

    code clean up

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 'use strict';
 
 var config = require('./config');
-var reviewer = require('./source/reviewer');
+var analyzer = require('./source/analyzer');
+
+function reviewer(config) {
+	if (!config) {
+		throw new Error('config is missing');
+	}
+
+	if (!config.paths.at) {
+		throw new Error('config.monitor section is missing');
+	}
+
+	return {
+		start: function () {
+			console.log('Start');
+
+			analyzer.getFiles(config.paths.at, function(er, files){
+				console.log('_________________________________________');
+				console.log('Files: \n');
+				console.log(files);
+				console.log('\nTotal: ' + files.length);
+				console.log('_________________________________________');
+			});
+		}
+	};
+}
 
 reviewer(config).start();
